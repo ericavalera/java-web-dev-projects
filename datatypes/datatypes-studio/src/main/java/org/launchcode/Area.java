8b5c196a5623632f07d1e493c4441e2e5ddca48d
ericavalera <cielosent@gmail.com>
2024-09-08 18:54:47 -0400

    Added bonus solution

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -3,9 +3,21 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[]args){
         Scanner input= new Scanner(System.in);
-        System.out.println("Enter a radius: ");
-        Double radius = input.nextDouble();
-        Double area= Circle.getArea(radius);
+        double radius;
+        do {
+            System.out.println("Enter a radius: ");
+            if (!input.hasNextDouble()) {
+                radius = 0;
+                input.next(); //clears the scanner
+                System.out.println("Radius must be a number.");
+            } else {
+                radius = input.nextDouble();
+                if (radius <= 0) {
+                    System.out.println("Radius must be a positive number.");
+                }
+            }
+        } while (radius <= 0);
+        double area= Circle.getArea(radius);
         System.out.println("The area of a circle of " + radius + " is " + area + "!");
     }
 
