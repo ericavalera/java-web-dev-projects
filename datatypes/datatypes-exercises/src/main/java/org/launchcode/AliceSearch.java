e04e0feae3c2f14d2a216a9b05400c4d0ef3cf86
ericavalera <cielosent@gmail.com>
2024-09-08 18:20:09 -0400

    Chapter 2 excersizes

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AliceSearch.java
@@ -0,0 +1,8 @@
+package org.launchcode;
+import java.util.Scanner;
+public class AliceSearch {
+    public static void main(String[]args){
+        Scanner input= new Scanner(System.in);
+    }
+
+}
