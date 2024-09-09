973f63fd648bc17d6754fb5a0bd6f1a033d27508
ericavalera <cielosent@gmail.com>
2024-09-08 20:05:13 -0400

    Added Alice search

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/AliceSearch.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AliceSearch.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class AliceSearch {
     public static void main(String[]args){
         String sentences = "Alice was beginning to get very tired of sitting by her sister" +
-                "on the bank, and having nothing to do; once or twice she had peeped into the" +
-                "book her sister was reading, but it had no pictures or conversations in it, 'and" +
-                "'what is the use of  a book', thought Alice 'without pictures or conversation?' ";
+                " on the bank, and having nothing to do; once or twice she had peeped into the" +
+                " book her sister was reading, but it had no pictures or conversations in it, 'and " +
+                "what is the use of  a book', thought Alice 'without pictures or conversation?' ";
 
         Scanner input= new Scanner(System.in);
         System.out.println("What word are you searching for?: ");
-        String word= input.next();
-        String wordUpper= word.toUpperCase();
+        String word = input.next();
+        String wordUpper = word.toUpperCase();
         String sentencesUpper = sentences.toUpperCase();
 
         if (sentencesUpper.contains(wordUpper)) {
@@ -18,6 +18,13 @@ public class AliceSearch {
         } else {
             System.out.println(word + " was not found in the Alice in Wonderland text.");
         }
+//        Bonus section
+        int index= sentencesUpper.indexOf(wordUpper);
+        int length= word.length();
+        System.out.println("Your search term first appears at index " + index + ".Your term is " + length + " characters long.");
+        String modifiedSentence = sentences.replace(word, "");
+        System.out.println(modifiedSentence);
+
     }
 }
 
