e4423b49d3e4b1421adee87ae9e7fb400c287b0c
ericavalera <cielosent@gmail.com>
2024-09-10 15:52:36 -0400

    updated

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
@@ -1,7 +1,7 @@
 package org.launchcode;
 public class Circle {
     public static Double getArea(Double radius) {
-        Double pi= 3.14;
+        Double pi= 3.14 ;
         return pi * radius * radius;
     }
 }
