966a0e3e6fe5b0b9871c1e691aa0cc3ba7a5e2e2
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-11 16:14:39 -0400

    Update ArrayListPractice.java

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayListPractice.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayListPractice.java
@@ -2,7 +2,25 @@ package org.launchcode;
 import java.util.ArrayList;
 
 public class ArrayListPractice {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        ArrayList<String> words = new ArrayList<>();
+
+        //evenNums sum
+        for(int integer : nums){
+            int total = 0;
+            if(integer % 2 == 0){
+                total += integer;
+            }
+           System.out.println(total);
+
+//                for loop, breakdown by characters
+//                and print words with character count 5
+            }
+        }
+
+    }
 
 
-}
+
 
