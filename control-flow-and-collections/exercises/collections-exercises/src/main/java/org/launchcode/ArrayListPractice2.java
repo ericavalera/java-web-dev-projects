15de16cdc70b46cca5814fc5bf4a2472cc821da3
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-11 00:01:12 -0400

    updates

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayListPractice2.java
+++
@@ -1,9 +0,0 @@
-package org.launchcode;
-
-import java.util.ArrayList;
-
-public class ArrayListPractice2 {
-    public static ArrayList<String> fiveLetter(){
-
-    };
-}
--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayPractice.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayPractice.java
@@ -1,12 +1,25 @@
 package org.launchcode;
 
+import java.util.Arrays;
+
 public class ArrayPractice {
     public static void main(String[] args) {
         int[] arrayNums = {1, 1, 2, 3, 5, 8};
+
+        String quote = "I would not, could not, in a box. I would not," +
+                " could not with a fox. I will not eat them in a house." +
+                " I will not eat them with a mouse.";
+
         for (int i : arrayNums) {
             if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
+        //split by words
+        String [] words = quote.split(" ");
+        System.out.println(Arrays.toString(words));
+        //split by sentences
+        String[] sentences = quote.split("\\.");
+        System.out.println(Arrays.toString(sentences));
     }
 }
