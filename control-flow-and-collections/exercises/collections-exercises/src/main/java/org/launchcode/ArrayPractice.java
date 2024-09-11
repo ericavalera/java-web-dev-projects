15b1c056932e0699435f1f163446b65684af4d15
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-10 20:12:15 -0400

    working on exercises

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayPractice.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayPractice.java
@@ -2,6 +2,11 @@ package org.launchcode;
 
 public class ArrayPractice {
     public static void main(String[] args) {
-    int[] arrayNums = {1, 1, 2, 3, 5, 8};
+        int[] arrayNums = {1, 1, 2, 3, 5, 8};
+        for (int i : arrayNums) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
