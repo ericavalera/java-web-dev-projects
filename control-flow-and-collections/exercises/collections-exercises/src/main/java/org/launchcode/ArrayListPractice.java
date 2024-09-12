8e771a1d9c16fe396d1e4099037d4878fbe01752
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-11 22:15:31 -0400

    Update ArrayListPractice.java

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayListPractice.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayListPractice.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class ArrayListPractice {
     public static void main(String[] args) {
-        ArrayList<Integer> nums = new ArrayList<>();
+       ArrayList<Integer> nums = new ArrayList<>();
         ArrayList<String> words = new ArrayList<>();
 
         //evenNums sum
