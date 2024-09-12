0285f2e5b5fe471932d3122b1be350ede42c366c
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-12 12:08:44 -0400

    Update ArrayListPractice.java

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayListPractice.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayListPractice.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 public class ArrayListPractice {
     public static void main(String[] args) {
        ArrayList<Integer> nums = new ArrayList<>();
-        ArrayList<String> words = new ArrayList<>();
+       ArrayList<String> words = new ArrayList<>();
+
+       words.add("cherry");
+       words.add("grape");
+       words.add("lemony");
+       words.add("violet");
 
         //evenNums sum
         for(int integer : nums){
@@ -12,10 +17,15 @@ public class ArrayListPractice {
             if(integer % 2 == 0){
                 total += integer;
             }
-           System.out.println(total);
+            System.out.println(total);
+                }
+//        for loop to print 5-letter words
+        for(String word: words){
+            if(word.length() == 5){
+                System.out.println(word);
+            }
 
-//                for loop, breakdown by characters
-//                and print words with character count 5
+
             }
         }
 
