23dd8a8b18d3c95ef3d33db7e939b3bb40b900b1
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-11 23:32:52 -0400

    Update Student.java

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -4,7 +4,34 @@ public class Student {
         private int numberOfCredits = 0;
         private double gpa = 0.0;
 
-        // Drop your getters and setters below for the Student class.
+//Drop your getters and setters below for the Student class.
+        public String getName(){
+                return name;
+        }
+        public void setName(String aName){
+                name = aName;
+        }
+
+        public int getStudentId (){
+                return studentId;
+        }
+        public void setStudentId(int aStudentId){
+                studentId = aStudentId;
+        }
+
+        public int getNumberOfCredits(){
+                return numberOfCredits;
+        }
+        public void setNumberOfCredits (int aNumberOfCredits){
+                numberOfCredits = aNumberOfCredits;
+        }
+        public double getGpa(){
+                return gpa;
+        }
+        public void setGpa(double aGpa){
+                gpa = aGpa;
+        }
+
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
 }
