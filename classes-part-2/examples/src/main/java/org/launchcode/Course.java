b9cf33e9eea17542f09be1faa328d124ce9f9103
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-19 14:56:25 -0400

    updates

---
+++ classes-part-2/examples/src/main/java/org/launchcode/Course.java
@@ -0,0 +1,27 @@
+package org.launchcode;
+
+import java.util.Objects;
+
+public class Course {
+    private String title;
+    private int credits;
+    private String instructor;
+
+    public Course (String title, int credits, String instructor) {
+        this.title = title;
+        this.credits = credits;
+        this.instructor = instructor;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return credits == course.credits && Objects.equals(title, course.title) && Objects.equals(instructor, course.instructor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, credits, instructor);
+    }
+}
