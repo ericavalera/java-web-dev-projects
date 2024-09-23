7c79677721a47a1f673a4f99432d2973ed27ea01
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-23 19:50:09 -0400

    Updates

---
+++ inheritance/technology/src/main/java/Computer.java
@@ -0,0 +1,56 @@
+public class Computer {
+    private String model;
+    private String operatingSystem;
+    private String category = "Technology";
+    private boolean isDesktop;
+    private boolean heavy = false;
+
+    // constructor
+    public Computer(String model, String operatingSystem, String category, boolean isDesktop, boolean heavy){
+        this.model = model;
+        this.operatingSystem = operatingSystem;
+        this.category = category;
+        this.isDesktop = isDesktop;
+        this.heavy = heavy;
+    }
+    public String getModel(){
+        return model;
+    }
+    public void setModel(String aModel){
+        model = aModel;
+    }
+    public String getOperatingSystem(){
+        return operatingSystem;
+    }
+    public void setOperatingSystem(String aOperatingSystem) {
+        operatingSystem = aOperatingSystem;
+    };
+    public String getCategory(){
+        return category;
+    }
+    public boolean getIsDesktop(){
+        return isDesktop;
+
+    }
+    public void setIsDesktop(boolean aIsDesktop){
+        isDesktop = aIsDesktop;
+    }
+    public boolean getHeavy(){
+        return heavy;
+    }
+    public void setHeavy(boolean aHeavy){
+        heavy = aHeavy;
+    }
+// methods
+
+    public boolean isHeavy(){
+        if (isDesktop = true){
+            heavy = true;
+        }
+        return heavy = false;
+    }
+
+    public String noise (){
+        return "no noise";
+    }
+}
