b51c38d2fc71643126b28b93f2025ffbfb96d018
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-28 23:23:53 -0400

    Updated Computer.java constructor and added classes to studio

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -14,24 +14,27 @@ public class Main {
         ArrayList<org.launchcode.Cone> cones = menu.getCones();
         Comparator<Flavor> comparator = new FlavorComparator();
         Comparator<Cone> comparator1 = new ConeComparator();
+
+        // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
+
         flavors.sort(comparator);
+
+        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
+
         cones.sort(comparator1);
+
+        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
 
         for(Flavor flavor : flavors){
             System.out.println(flavor.getName());
-
         }
-
         for(Cone cone : cones){
             System.out.println(cone.getName() + " $"+ cone.getCost());
         }
 
 
 
-        // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
 }
