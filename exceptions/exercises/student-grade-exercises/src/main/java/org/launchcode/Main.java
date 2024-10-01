43995ca4c40588eb30d4922329c980fa883afffa
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-30 21:37:18 -0400

    Updates

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        Divide(5, 0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,15 +14,36 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        CheckFileExtension(studentFiles);
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static int Divide(int x, int y) {
+        int z = y / x;
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("'y' can't equal zero, try another number.");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+        return z;
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+//    TODO: fix this code, might need iteration
+    public static int CheckFileExtension(HashMap<String, String> fileName) {
+        int studentPoints = 0;
+        if (fileName.containsKey(".java")) {
+            studentPoints += 1;
+        } else if (fileName.containsKey(null)) {
+            try {
+                throw new StudentException("Needs to have a file name.");
+            } catch (StudentException error) {
+                error.printStackTrace();
+
+            }
+        } else {
+            studentPoints += 0;
+        }
+        return studentPoints;
     }
 }
